8a470691f9867b4b406e8a85c01020d5aa9619af
kuldeep-seek <kbishnoi@seekasia.com>
2024-07-14 21:37:19 +0530

    authmiddleware is included

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const { signUp, signIn } = require('../controllers/authController');
-
+const {authMiddleware} = require('../middleware/authMiddleware')
 const router = express.Router();
 
 router.post('/signup', signUp);
-router.post('/signin', signIn);
+router.post('/signin',authMiddleware,signIn);
 
 module.exports = router;
